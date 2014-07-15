f38f2a6b795c4ca4ffdfe6f881d976f70976b108
MaR1oC <mrmarios97@gmail.com>
2014-07-15 09:38:11 +0300

    minor changes

--- src/lib/fish.py
+++ src/lib/fish.py
@@ -6,14 +6,31 @@ class Fish:
 		
 	def __str__(self):
 		return self.name
-
-class Database:
-	pass 
 	
 class FishDatabase(Database):
 	
 	def __init__(self, imembers=[]):
 		self.members = [] + imembers
+		#super(Database, self).__init__(self.__name__)
 	
 	def append_member(self, m):
 		self.members.append(m)
+
+
+class Database:
+	
+	def __init__(self, filename):
+		import sqlite3 as db
+		self.connection = db.connect(filename)
+		self.filename = filename
+		self.cursor = self.connection.cursor()
+		self.tables = []
+		
+	def execute(self, cmd):
+		self.cursor.execute(cmd)
+			
+#class Table:
+#	def __init__(self,statement, c):
+#		c.execute('CREATE TABLE ' + statement)		
+#	def getData(self, keyword):
+		
