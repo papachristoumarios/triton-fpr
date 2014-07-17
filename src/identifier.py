d14ab79f57652d72f2f94eda354709c73636c257
MaR1oC <mrmarios97@gmail.com>
2014-07-17 13:54:49 +0300

    many many changes

--- src/identifier.py
+++ src/identifier.py
@@ -1,5 +1,8 @@
 from lib.fish import *
-from scripts.detect import detect2
+import sys
+
+#sys.path.append('scripts/')
+import scripts.detector as detector 
 
 #scripts.detect.detect2 returns:
 # elements[4] [0]: image, [1]: NOF, [2]: true if found, [3]:coordinates
@@ -7,31 +10,34 @@ from scripts.detect import detect2
 def identify(image, base, detectCharacteristics = False):
 	artifacts = {}
 	for f in base:
-		elems = detect2(image,f.bodycascade)
+		elems = detector.detect2(image,f.bodycascade)
 		if elems[2]:
 			print 'Found: {0} Quantity: {1}'.format(str(f),elems[1])
 			print 'Coordinates:' + str(elems[3])
 			artifacts[str(f)] = elems[1]
 			if detectCharacteristics: #characteristics identification
-				for i in range(0, len(elems[3]):
+				for i in range(0, len(elems[3])):
 					x = elems[3][i][0]
 					y = elems[3][i][1]
 					w = elems[3][i][2]
 					h = elems[3][i][3]
 					roi = image[x: x+w, y: y+h]
 					for j in len(0,f.fcascades):
-						roi_elems = detect2(image,f.fcascades[j])
+						roi_elems = detector.detect2(image,f.fcascades[j])
 						print '{0} --> Feature: {1} Quantity: {2}'.format(i, j, roi_elems[1])
 						print 'Coordinates: ' + str(roi_elems[1])
-					
-					
+	return artifacts									
 				
-			
-			
+class ComplexIdentification:
+	
+	def calculate_probability(img_freq,geo_freq):
+		from math import sqrt
+		return sqrt(img_freq**2 + geo_freq**2)		
 			
 #TODO Add main method for tests
 
-
+if __name__ == '__main__':
+	pass 
 
 
 
